bdb378dd8a4fa032cc69f243a8789483c4a8ea43
Maksym Kononenko <kononenkomg@gmail.com>
2024-03-22 23:17:56 -0700

    add stage env

--- src/server/helpers.ts
+++ src/server/helpers.ts
@@ -3,12 +3,14 @@ import { createTRPCProxyClient } from '@trpc/client';
 import { AppRouter } from './routers/_app';
 import superjson from 'superjson'
 import { httpBatchLink } from '@trpc/client/links/httpBatchLink';
+import dotenv from 'dotenv';
+dotenv.config();
 
 
 export const proxyClient = createTRPCProxyClient<AppRouter>({
   links: [
     httpBatchLink({
-      url: 'http://localhost:3000/api/trpc',
+      url: process.env.STAGE === 'development' ? 'http://localhost:3000/api/trpc' : 'https://stock-status-ruddy.vercel.app/api/trpc',
     }),
   ],
   transformer: superjson,
